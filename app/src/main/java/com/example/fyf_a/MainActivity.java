42edf4562bfad93e5e3d718783692a5b70a8ce9a
bcolitti732 <bruno.colitti@estudiantat.upc.edu>
2024-12-13 01:26:18 +0100

    commit

---
+++ app/src/main/java/com/example/fyf_a/MainActivity.java
@@ -0,0 +1,101 @@
+package com.example.fyf_a;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.os.Handler;
+import android.view.View;
+import android.widget.EditText;
+
+import com.example.fyf_a.RegisterActivity;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.i("testProjecte", "Starting app");
+    }
+
+    public void goToRegisterActivity(View vista) {
+        Intent intento = new Intent(this, RegisterActivity.class);
+        startActivity(intento);
+    }
+
+    public void Login(View view) {
+        new Thread(new Runnable() {
+            EditText Username = (EditText) findViewById(R.id.nameLbl);
+            EditText Password = (EditText) findViewById(R.id.passwordLbl);
+            InputStream stream = null;
+            String result = null;
+            Handler handler = new Handler();
+
+            public void run() {
+                if (!Username.getText().toString().isEmpty() && !Password.getText().toString().isEmpty()) {
+                    try {
+                        String query = String.format("http://192.168.1.132:9000/Android/loginUser?username=" + Username.getText().toString() + "&password=" + Password.getText().toString());
+                        URL url = new URL(query);
+                        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                        conn.setReadTimeout(10000);
+                        conn.setConnectTimeout(15000);
+                        conn.setRequestMethod("GET");
+                        conn.setDoInput(true);
+                        conn.connect();
+                        stream = conn.getInputStream();
+
+                        BufferedReader reader = new BufferedReader(new InputStreamReader(stream));
+                        StringBuilder sb = new StringBuilder();
+                        String line;
+                        while ((line = reader.readLine()) != null) {
+                            sb.append(line);
+                        }
+                        result = sb.toString();
+                        conn.disconnect();
+
+                        Log.i("testProjecte", "result value is" + result);
+                        if (result.equals("1")) {
+                            // Aqui ira a la pagina con los pisos pero no se que poner
+                        } else {
+                            handler.post(new Runnable() {
+                                public void run() {
+                                    MainActivity.this.runOnUiThread(new Runnable() {
+                                        @Override
+                                        public void run() {
+                                            AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                                            builder.setMessage("Username or password not found")
+                                                    .setTitle("FurHub")
+                                                    .setPositiveButton("OK", null);
+                                            builder.create().show();
+                                        }
+                                    });
+                                }
+                            });
+                        }
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                } else {
+                    handler.post(new Runnable() {
+                        public void run() {
+                            AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                            builder.setMessage("Please fill in all fields")
+                                    .setTitle("FurHub")
+                                    .setPositiveButton("OK", null);
+                            builder.create().show();
+                        }
+                    });
+                }
+            }
+        }).start();
+    }
+}
---
+++ app/src/main/java/com/example/fyf_a/RegisterActivity.java
@@ -0,0 +1,199 @@
+package com.example.fyf_a;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.os.Handler;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+    public void Register_function(View view) {
+
+        new Thread(new Runnable() {
+            EditText Username = (EditText) findViewById(R.id.nameLbl);
+            EditText Password = (EditText) findViewById(R.id.passwordLbl);
+            EditText Confirm_Password = (EditText) findViewById(R.id.confirmpasswordLbl);
+            EditText Mail = (EditText) findViewById(R.id.mailLbl);
+            EditText Age = (EditText) findViewById(R.id.ageLbl);
+            EditText Nationality = (EditText) findViewById(R.id.nationalityLbl);
+            EditText Occupation = (EditText) findViewById(R.id.occupationLbl);
+            ImageView Pass_Error = (ImageView) findViewById(R.id.pass_error);
+            ImageView Pass_Error2 = (ImageView) findViewById(R.id.pass_error2);
+            ImageView Name_Error = (ImageView) findViewById(R.id.name_error);
+
+            InputStream stream = null;
+            String result = null;
+            Handler handler = new Handler();
+            public void run() {
+                if(!Username.getText().toString().isEmpty() && !Password.getText().toString().isEmpty() && !Confirm_Password.getText().toString().isEmpty()) {
+                    try {
+                        String query = String.format("http://192.168.1.132:9000/Android/registerUser");
+                        URL url = new URL(query);
+                        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                        conn.setReadTimeout(10000 );
+                        conn.setConnectTimeout(15000 /* milliseconds */);
+                        conn.setRequestMethod("POST");
+                        conn.setDoInput(true);
+                        conn.setDoOutput(true);
+                        conn.connect();
+                        if(Password.getText().toString().equals(Confirm_Password.getText().toString()))
+                        {
+                            // sending parameters to login function
+
+                            String params = "username="+Username.getText().toString()+"&password="+Password.getText().toString()+"&mail="+Mail.getText().toString()+"&age="+Age.getText().toString()+"&nationality="+Nationality.getText().toString()+"&occupation="+Occupation.getText().toString();
+                            Log.i("serverTest ", params);
+
+                            OutputStream os = conn.getOutputStream();
+                            BufferedWriter writer = new BufferedWriter(
+                                    new OutputStreamWriter(os, "UTF-8"));
+                            writer.write(params.toString());
+
+                            writer.flush();
+                            writer.close();
+                            os.close();
+
+                            Log.i("serverTest ", "waiting for response...");
+
+                            //server response
+                            stream = conn.getInputStream();
+                            BufferedReader reader = null;
+                            StringBuilder sb = new StringBuilder();
+                            reader = new BufferedReader(new InputStreamReader(stream));
+                            String line = null;
+                            while ((line = reader.readLine()) != null) {
+                                sb.append(line);
+                            }
+                            result = sb.toString();
+                            conn.disconnect();
+                            //Correct code
+
+                            if(result.equals("1"))
+                            {
+                                Intent intento= new Intent(RegisterActivity.this , com.example.fyf_a.MainActivity.class);
+                                startActivity(intento);
+                            }
+                            else{
+                                handler.post(new Runnable() {
+                                    public void run() {
+                                        if(Name_Error.getVisibility()==View.INVISIBLE){
+                                            Name_Error.setVisibility(View.VISIBLE);
+                                        }
+                                        if(Pass_Error.getVisibility()==View.VISIBLE){
+                                            Pass_Error.setVisibility(View.INVISIBLE);
+                                        }
+                                        if(Pass_Error2.getVisibility()==View.VISIBLE){
+                                            Pass_Error2.setVisibility(View.INVISIBLE);
+                                        }
+
+                                        RegisterActivity.this.runOnUiThread(new Runnable() {
+                                            @Override
+                                            public void run() {
+                                                AlertDialog.Builder builder = new AlertDialog.Builder(RegisterActivity.this);
+                                                builder
+                                                        .setMessage("The username is not available :(")
+                                                        .setTitle("FurHub")
+                                                        .setPositiveButton("OK", null);
+
+                                                builder.create().show();
+                                            }
+                                        });
+                                    }
+                                });
+                            }
+                        }
+                        else{
+                            handler.post(new Runnable() {
+                                public void run() {
+                                    if(Pass_Error.getVisibility()==View.INVISIBLE){
+                                        Pass_Error.setVisibility(View.VISIBLE);
+                                    }
+                                    if(Pass_Error2.getVisibility()==View.INVISIBLE){
+                                        Pass_Error2.setVisibility(View.VISIBLE);
+                                    }
+                                    if(Name_Error.getVisibility()==View.VISIBLE){
+                                        Name_Error.setVisibility(View.INVISIBLE);
+                                    }
+                                }
+                            });
+                        }
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+                else{
+                    if(Username.getText().toString().isEmpty()){
+                        if(Name_Error.getVisibility()==View.INVISIBLE) {
+                            handler.post(new Runnable() {
+                                public void run() {
+                                    Name_Error.setVisibility(View.VISIBLE);}});
+                        }
+                    }
+                    else{ handler.post(new Runnable() {
+                        public void run() {
+                            Name_Error.setVisibility(View.INVISIBLE);
+                        }
+                    });
+                    }
+                    if(Password.getText().toString().isEmpty()){
+                        if(Pass_Error.getVisibility()==View.INVISIBLE)
+                        {
+                            handler.post(new Runnable() {
+                                public void run() {
+                                    Pass_Error.setVisibility(View.VISIBLE);
+                                }
+                            });
+                        }
+                    }
+                    else{ handler.post(new Runnable() {
+                        public void run() {
+                            Pass_Error.setVisibility(View.INVISIBLE);
+                        }
+                    });
+                    }
+                    if(Confirm_Password.getText().toString().isEmpty()){
+                        if(Pass_Error2.getVisibility()==View.INVISIBLE)
+                        {
+                            handler.post(new Runnable() {
+                                public void run() {
+                                    Pass_Error2.setVisibility(View.VISIBLE);
+                                }
+                            });
+                        }
+                    }
+                    else{ handler.post(new Runnable() {
+                        public void run() {
+                            Pass_Error2.setVisibility(View.INVISIBLE);
+                        }
+                    });
+                    }
+
+                };
+            }
+
+        }).start();
+    }
+
+
+}
+

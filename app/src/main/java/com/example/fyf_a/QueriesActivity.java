df00cd89e6ee15287fd25397fae5e99bf23eddbb
bcolitti732 <bruno.colitti@estudiantat.upc.edu>
2025-01-18 16:28:51 +0100

    query2!!

--- app/src/main/java/com/example/fyf_a/QueriesActivity.java
+++ app/src/main/java/com/example/fyf_a/QueriesActivity.java
@@ -3,12 +3,18 @@ package com.example.fyf_a;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+
+import org.json.JSONArray;
+import org.json.JSONException;
 
 import java.io.BufferedReader;
 import java.io.InputStream;
@@ -16,6 +22,8 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLEncoder;
+import java.util.ArrayList;
+import java.util.List;
 
 public class QueriesActivity extends AppCompatActivity {
     @Override
@@ -80,6 +88,74 @@ public class QueriesActivity extends AppCompatActivity {
                                     .setPositiveButton("OK", null);
 
                             builder.create().show();
+                        }
+                    });
+                }
+            }
+        }).start();
+    }
+    public void getRatersList(View view) {
+        new Thread(new Runnable() {
+            EditText flatAddress = (EditText) findViewById(R.id.addressLbl);
+            ListView ratersListView = (ListView) findViewById(R.id.ratersList);
+            InputStream stream = null;
+            String result = null;
+            Handler handler = new Handler();
+
+            public void run() {
+                if (!flatAddress.getText().toString().isEmpty()) {
+                    try {
+                        String address = flatAddress.getText().toString();
+                        String query = String.format("http://192.168.56.1:9000/Android/getRatersList?address=%s", URLEncoder.encode(address, "UTF-8"));
+                        URL url = new URL(query);
+                        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                        conn.setReadTimeout(10000);
+                        conn.setConnectTimeout(15000); // milliseconds
+                        conn.setRequestMethod("GET");
+                        conn.setDoInput(true);
+                        conn.connect();
+
+                        stream = conn.getInputStream();
+                        BufferedReader reader = new BufferedReader(new InputStreamReader(stream));
+                        StringBuilder sb = new StringBuilder();
+                        String line;
+
+                        while ((line = reader.readLine()) != null) {
+                            sb.append(line);
+                        }
+                        result = sb.toString();
+                        conn.disconnect();
+
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                if (result.equals("Flat not found") || result.equals("No ratings found for this flat")) {
+                                    Toast.makeText(QueriesActivity.this, result, Toast.LENGTH_LONG).show();
+                                } else {
+                                    try {
+                                        JSONArray jsonArray = new JSONArray(result);
+                                        List<String> raters = new ArrayList<>();
+                                        for (int i = 0; i < jsonArray.length(); i++) {
+                                            raters.add(jsonArray.getString(i));
+                                        }
+                                        ArrayAdapter<String> adapter = new ArrayAdapter<>(QueriesActivity.this, android.R.layout.simple_list_item_1, raters);
+                                        ratersListView.setAdapter(adapter);
+                                    } catch (JSONException e) {
+                                        e.printStackTrace();
+                                        Toast.makeText(QueriesActivity.this, "Error parsing data", Toast.LENGTH_LONG).show();
+                                    }
+                                }
+                            }
+                        });
+
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                } else {
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            Toast.makeText(QueriesActivity.this, "Please enter a flat address", Toast.LENGTH_LONG).show();
                         }
                     });
                 }
